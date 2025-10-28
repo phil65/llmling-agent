c5a4e7f3b604a6db79652a7023e252a75e073b84
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-10-28 20:41:34 +0100

    chore: cleanup

--- src/llmling_agent/__main__.py
+++ src/llmling_agent/__main__.py
@@ -6,9 +6,7 @@ from llmling.cli.utils import get_command_help
 import typer as t
 
 from llmling_agent_cli.agent import add_agent_file, list_agents, set_active_file
-from llmling_agent_cli.chat import chat_command
 from llmling_agent_cli.history import history_cli
-from llmling_agent_cli.quickstart import quickstart_command
 from llmling_agent_cli.run import run_command
 from llmling_agent_cli.serve_acp import acp_command
 from llmling_agent_cli.serve_api import api_command
@@ -27,8 +25,6 @@ cli.command(name="add")(add_agent_file)
 cli.command(name="run")(run_command)
 cli.command(name="list")(list_agents)
 cli.command(name="set")(set_active_file)
-cli.command(name="chat")(chat_command)
-cli.command(name="quickstart")(quickstart_command)
 cli.command(name="watch")(watch_command)
 cli.command(name="serve-acp")(acp_command)
 cli.command(name="serve-mcp")(serve_command)
