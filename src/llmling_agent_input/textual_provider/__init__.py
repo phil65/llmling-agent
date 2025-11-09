8decf34cd82b5f9c6ade998e378b74c5b70a1b2a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-11-09 05:04:09 +0100

    chore: rem textual input provider

--- src/llmling_agent_input/textual_provider/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Textual input provider package."""
-
-from __future__ import annotations
-
-from llmling_agent_input.textual_provider.provider import TextualInputProvider
-
-__all__ = ["TextualInputProvider"]
