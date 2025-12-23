0f3a9cc5ca92d38bc18075114d5bbf55a228da9c
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-12-23 22:57:10 +0100

    chore: typing

--- src/agentpool_toolsets/builtin/execution_environment.py
+++ src/agentpool_toolsets/builtin/execution_environment.py
@@ -87,8 +87,7 @@ class ExecutionEnvironmentTools(ResourceProvider):
             async for event in self.get_env(agent_ctx).stream_code(code):
                 match event:
                     case ProcessStartedEvent(process_id=pid, command=cmd):
-                        process_id = pid
-                        await agent_ctx.events.process_started(process_id, cmd, success=True)
+                        await agent_ctx.events.process_started(pid, cmd, success=True)
                     case OutputEvent(data=data):
                         output_parts.append(data)
                         if process_id:
