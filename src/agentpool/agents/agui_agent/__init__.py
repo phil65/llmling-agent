35d1c86546281b7c00813974befaafae8c07f09a
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-12-24 08:52:42 +0100

    chore: cleanup

--- src/agentpool/agents/agui_agent/__init__.py
+++ src/agentpool/agents/agui_agent/__init__.py
@@ -10,13 +10,8 @@ from agentpool.agents.agui_agent.agui_converters import (
 from agentpool.agents.agui_agent.chunk_transformer import ChunkTransformer
 
 __all__ = [
-    # Main agent
     "AGUIAgent",
-    # Chunk transformation
     "ChunkTransformer",
-    # Extended event type
-    "Event",
-    # Converters
     "ToolCallAccumulator",
     "agui_to_native_event",
     "to_agui_input_content",
