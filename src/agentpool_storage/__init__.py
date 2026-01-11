2910ab6cd4b562f76c03f1cac92419da2e2b70af
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2026-01-11 08:12:16 +0100

    rem some top-level imports

--- src/agentpool_storage/__init__.py
+++ src/agentpool_storage/__init__.py
@@ -1,8 +1,6 @@
 """Storage provider package."""
 
 from agentpool_storage.base import StorageProvider
-from agentpool_storage.claude_provider import ClaudeStorageProvider
-from agentpool_storage.opencode_provider import OpenCodeStorageProvider
 from agentpool_storage.project_store import (
     ProjectStore,
     detect_project_root,
@@ -13,8 +11,6 @@ from agentpool_storage.project_store import (
 from agentpool_storage.session_store import SQLSessionStore
 
 __all__ = [
-    "ClaudeStorageProvider",
-    "OpenCodeStorageProvider",
     "ProjectStore",
     "SQLSessionStore",
     "StorageProvider",
